bc3b62b6c6bee6e5b3313627571b3306c058c3a3
NFarhan11 <najmifarhan9903@gmail.com>
2025-09-16 08:55:55 +0800

    setup frontend

---
+++ frontend/app/pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Hello World {{ version }}</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+const version = useRuntimeConfig().public.version;
+</script>
+
+<style scoped>
+h1 {
+  font-size: 3rem;
+}
+</style>
